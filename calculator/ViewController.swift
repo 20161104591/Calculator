b4076f65ba1713831ea42e54d41e1ae0d4c736a5
20161104591 <xyf@mac23.local>
2018-10-10 10:02:07 +0800

    Initial Commit

---
+++ calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  calculator
+//
+//  Created by XYF on 2018/10/10.
+//  Copyright © 2018年 XYF. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
